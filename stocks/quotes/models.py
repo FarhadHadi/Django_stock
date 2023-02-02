5c026ed803318e142ac270e49554ffb6c00ccabe
Farhad <farhadhadi1991@gmail.com>
2023-02-02 14:19:44 +0000

    steg

--- stocks/quotes/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Stock(models.Model):
-    ticker = models.CharField(max_length=10) #CharField is type of data
-
-    def __str__(self):
-        return self.ticker
-
-    
